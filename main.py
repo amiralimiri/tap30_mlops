ff3b21fbf62fde2edd2a2b2076c109472d7d6036
amiralimiri <amiralimiri3100@gmail.com>
2025-11-12 12:46:00 +0330

    add: data processing

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from src.config_reader import read_config
 from src.data_ingestion import DataIngestion
+from src.data_processing import DataProcessing
 
-data_ingestion = DataIngestion(read_config("config/config.yaml"))
-data_ingestion.run()
+# data_ingestion = DataIngestion(read_config("config/config.yaml"))
+# data_ingestion.run()
+
+data_processing = DataProcessing(read_config("config/config.yaml"))
+data_processing.run()
