b0679eb71cf66481ab6d9aaf9d7848c6e58d1fbe
amiralimiri <amiralimiri3100@gmail.com>
2025-11-14 09:59:17 +0330

    add: train, save, track experiment

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from src.config_reader import read_config
 from src.data_ingestion import DataIngestion
 from src.data_processing import DataProcessing
+from src.model_training import ModelTraining
 
-# data_ingestion = DataIngestion(read_config("config/config.yaml"))
+config = read_config("config/config.yaml")
+
+# data_ingestion = DataIngestion(config)
 # data_ingestion.run()
 
-data_processing = DataProcessing(read_config("config/config.yaml"))
-data_processing.run()
+# data_processing = DataProcessing(config)
+# data_processing.run()
+
+model_training = ModelTraining(config)
+model_training.run()
